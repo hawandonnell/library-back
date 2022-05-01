dc59c95e87e999ff7ff69f52bd0783a72b0a9244
Altair <tair_ermekbai@mail.ru>
2022-05-01 20:40:49 +0600

    Migrations deleted

--- prisma/migrations/20220429125205_changed/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `birthAt` on the `Author` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Author" DROP COLUMN "birthAt",
-ADD COLUMN     "birthAt" INTEGER NOT NULL;
